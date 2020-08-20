134f8075e416cf0aa1bfc64fff2552342a0c2b2e
Frederik De Brouwer <frederikdebrouwer@gmail.com>
2020-08-20 14:34:08 +0200

    Edit redirect URI

--- application.py
+++ application.py
@@ -55,8 +55,8 @@ SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
 
 # ------------- Server-side Parameters -------------
 CLIENT_SIDE_URL = "https://my-spotify-statistics.herokuapp.com"
-PORT = 8080
-REDIRECT_URI = "{}:{}/callback/q".format(CLIENT_SIDE_URL, PORT)
+#PORT = 8080
+REDIRECT_URI = "{}/callback/q".format(CLIENT_SIDE_URL)
 SCOPE = "user-follow-read user-library-read user-read-email user-read-private user-read-recently-played user-top-read playlist-read-private playlist-modify-public playlist-modify-private streaming"
 STATE = ""
 SHOW_DIALOG_bool = True
