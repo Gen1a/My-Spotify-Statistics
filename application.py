56e9886a4014cf48a0395b373f84487bcb99411d
Frederik De Brouwer <frederikdebrouwer@gmail.com>
2020-08-20 13:41:51 +0200

    Removed port + edited Procfile formatting

--- application.py
+++ application.py
@@ -478,4 +478,4 @@ def errorhandler(e):
 
 if __name__ == "__main__":
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(debug=True, port=PORT, threaded=True)
+    app.run(debug=True, threaded=True)
