1409b94903077ec465f028bc24dacd67690ccd84
Frederik De Brouwer <frederikdebrouwer@gmail.com>
2020-08-21 23:15:59 +0200

    Added favicon white version

--- application.py
+++ application.py
@@ -130,7 +130,7 @@ def index():
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+                               'favicon-w.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route("/login")
