acc14e5dce803337d1f7c444d06da5a74a1c80d7
Frederik De Brouwer <frederikdebrouwer@gmail.com>
2020-08-21 23:00:35 +0200

    Added /favicon.ico route

--- application.py
+++ application.py
@@ -127,6 +127,12 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
 @app.route("/login")
 def login():
     """ Shows login page """
